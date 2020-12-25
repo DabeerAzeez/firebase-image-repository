344b269cdc3a68728c7f0dc3653d6e48c25c4c60
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-12-24 21:54:00 -0500

    Loop through Firebase Storage images

--- public/main.js
+++ public/main.js
@@ -1,13 +1,21 @@
 const storageRef = firebase.storage().ref();
 var provider = new firebase.auth.GoogleAuthProvider();
-var signedIn = false
+var signedIn = false;
+var image_counter = 0;
+var images = [];
 
 function uploadImage() {
     const file = document.getElementById('image_upload').files[0];
+
+    if (!file) {
+        alert("No file chosen!")
+        return
+    }
+
     const fileRef = storageRef.child(file.name)
 
     fileRef.put(file).then(snapshot => {
-        console.log('Image uploaded!');
+        alert('Image uploaded successfully!');
     }).catch(error => {
         alert("Error occurred while uploading, please ensure you are signed in." + "\n\n" + error)
     });
@@ -20,6 +28,7 @@ function signIn() {
             var user = result.user;
             signedIn = true
             document.getElementById('sign-in').innerHTML = "Sign Out";
+            alert("Signed in successfully")
         }).catch(function(error) {
             var errorCode = error.code;
             var errorMessage = error.message;
@@ -27,8 +36,6 @@ function signIn() {
             var credential = error.credential;
             alert("There was an error signing you in. Please contact the developer of this crude web app immediately.")
         });
-
-        alert("Signed in successfully")
     } else {
         firebase.auth().signOut().then(function() {
             signedIn = false
@@ -38,5 +45,26 @@ function signIn() {
             alert("There was an error signing you out. Please contact the developer of this crude web app immediately.")
         });
     }
-
 }
+
+function setImage(index) {
+    images[index].getDownloadURL().then(url => {
+        document.getElementById("selected_image").src = url
+    });
+}
+
+function nextImage() {
+    setImage(image_counter)
+    image_counter++
+
+    if (image_counter > images.length - 1) {
+        image_counter = 0
+    }
+}
+
+storageRef.listAll().then(res => {
+    images = res.items
+    setImage(0)
+    console.log("Finished loading images")
+});
+
